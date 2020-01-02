dd396d58efbedfa8058ac0e98d131788815e5ee9
ymgyt <yamaguchi7073xtt@gmail.com>
2020-01-02 21:41:00 +0900

    display zero input error message

--- src/error.rs
+++ src/error.rs
@@ -10,7 +10,11 @@ pub enum Error {
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "error:")
+        use Error::*;
+        match self {
+            InputRequired => write!(f, "input required"),
+            _ => write!(f, "{}", self),
+        }
     }
 }
 
